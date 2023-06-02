46a33b20ad55785790865308e4d9164098a8d838
ITPLUS Academy <thanghoang064@gmail.com>
2023-06-02 19:22:51 +0700

    update project

---
+++ base_mvc/env.php
@@ -0,0 +1,8 @@
+<?php
+// Đây là nơi sẽ đặt toàn bộ những biến chung của dự án
+const DBNAME = "we17316";
+const DBUSER = "root";
+const DBPASS = "";
+const DBCHARSET = "utf8";
+const DBHOST = "127.0.0.1";
+
